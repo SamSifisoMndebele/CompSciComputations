36d7721e7178a4b5f7d09498784507e05e057885
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2024-05-17 16:40:05 +0200

    v1.0.40517

---
+++ logic/src/main/java/com/compscicomputations/logic/num_systems/bases/BaseError.kt
@@ -0,0 +1,23 @@
+package com.compscicomputations.logic.num_systems.bases
+
+enum class BaseError(val message: String) {
+    SIZE_ERROR("Size error"),
+    INVALID_DECIMAL("Invalid decimal number"),
+    INVALID_BINARY("Invalid binary number"),
+    INVALID_OCT("Invalid octal number"),
+    INVALID_HEX("Invalid hexadecimal number"),
+    INVALID_NUM("Invalid number");
+
+    companion object {
+        infix fun errorOf(base: Int) : BaseError {
+            return when(base) {
+                0 -> SIZE_ERROR
+                2 -> INVALID_BINARY
+                8 -> INVALID_OCT
+                10 -> INVALID_DECIMAL
+                16 -> INVALID_HEX
+                else -> INVALID_NUM
+            }
+        }
+    }
+}
