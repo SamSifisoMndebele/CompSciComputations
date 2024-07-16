58d63e653754a43afeaf92c953e13447fd9b460f
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2024-07-16 02:26:26 +0200

    0.1.0

--- src/main/kotlin/com/compscicomputations/firebase/FirebasePrincipal.kt
+++
@@ -1,9 +0,0 @@
-package com.compscicomputations.firebase
-
-import io.ktor.server.auth.Principal
-
-data class FirebasePrincipal(
-    val uid: String,
-    val email: String,
-    val isAdmin: Boolean
-): Principal
