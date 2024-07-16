0b5c0b8a123bf425c4da8a40bf7a58a52df18c28
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2024-07-16 15:15:54 +0200

    0.0.6

--- extensions.sql
+++ extensions.sql
@@ -1,19 +1,34 @@
-CREATE SCHEMA extensions;
+CREATE SCHEMA if not exists ext;
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto
-    SCHEMA extensions
+    SCHEMA ext
     CASCADE;
 
-CREATE EXTENSION IF NOT EXISTS citext
-    SCHEMA extensions
-    CASCADE;
-
-
-
-CREATE EXTENSION IF NOT EXISTS pgjwt
-    SCHEMA extensions
-    CASCADE;
+-- CREATE EXTENSION IF NOT EXISTS citext
+--     SCHEMA ext
+--     CASCADE;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
-    SCHEMA extensions
+    SCHEMA ext
     CASCADE;
+
+
+-- Custom EXTENSIONS
+create or replace function ext.isNullOrBlank(value text)
+returns boolean
+    language plpgsql
+as
+$code$
+begin
+    return coalesce(length(trim(value)) = 0, true);
+end;
+$code$;
+create or replace function ext.isNullOrEmpty(value text)
+returns boolean
+    language plpgsql
+as
+$code$
+begin
+    return coalesce(length(value) = 0, true);
+end;
+$code$;
