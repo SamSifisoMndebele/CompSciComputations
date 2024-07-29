c8d0b76a84273e10acd7ae3898b890fa041a347c
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2024-07-29 20:04:45 +0200

    0.2.3

--- src/main/kotlin/com/compscicomputations/database/_/extensions.sql
+++ src/main/kotlin/com/compscicomputations/database/_/extensions.sql
@@ -8,9 +8,9 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto
 --     SCHEMA ext
 --     CASCADE;
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
-    SCHEMA ext
-    CASCADE;
+-- CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
+--     SCHEMA ext
+--     CASCADE;
 
 
 -- Custom EXTENSIONS
@@ -18,32 +18,32 @@ create or replace function ext.isNullOrBlank(value text)
 returns boolean
     language plpgsql
 as
-$code$
+$$
 begin
     return coalesce(length(trim(value)) = 0, true);
 end;
-$code$;
+$$;
 
 create or replace function ext.isNullOrEmpty(value text)
 returns boolean
     language plpgsql
 as
-$code$
+$$
 begin
     return coalesce(length(value) = 0, true);
 end;
-$code$;
+$$;
 
 create or replace function ext.gen_random_otp(_digits int default 6)
 returns text
     strict
     language plpgsql
 as
-$code$
+$$
 begin
     return floor((random() * 9 + 1) * power(10, _digits - 1))::text;
 end;
-$code$;
+$$;
 
 create or replace function ext.nowSAST()
 returns timestamp
@@ -51,8 +51,8 @@ returns timestamp
     strict
     language plpgsql
 as
-$code$
+$$
 begin
     return (now() at time zone 'Africa/Johannesburg');
 end;
-$code$;
+$$;
